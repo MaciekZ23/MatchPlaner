35e536968ced95690686624e7985c37c593984a6
MaciekZ23 <maciekz232002@o2.pl>
2025-08-23 22:32:11 +0200

    Zaladowanie druzyn mockowanych api, dodanie mockowanych danych druzyn

--- src/app/core/mocks/mock-tournament.api.ts
+++ src/app/core/mocks/mock-tournament.api.ts
@@ -6,11 +6,10 @@ import { MatchStatus } from '../types';
 
 @Injectable()
 export class MockTournamentApi implements ITournamentApi {
-  // MOCK: turniej + stage + grupy
   private readonly STAGE_GRP = 'STAGE-GRP-1';
   private tournament: Tournament = {
     id: 't1',
-    name: 'Liga testowa',
+    name: 'Liga halowa Toruń 2026 test',
     mode: 'LEAGUE',
     description:
       'Halowa Liga Piłki Nożnej Toruń to coroczne wydarzenie sportowe, które gromadzi drużyny z całego regionu. Edycja 2026 zapowiada się wyjątkowo emocjonująco – ponad 20 zespołów, setki kibiców i pasja, która napędza każdą akcję na boisku.',
@@ -21,47 +20,174 @@ export class MockTournamentApi implements ITournamentApi {
     endDate: '2025-08-31',
     venue: 'Arena Toruń',
     venueAddress: 'ul. Józefa Bema 73/89, 87-100 Toruń',
-    venueImageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/c/c3/Hala_sportowa_Torun_09_2014.JPG',
+    venueImageUrl: 'assets/MIEJSCA/ARENA-TORUN.jpg',
     groups: [
-      { id: 'A', name: 'Grupa A', teamIds: ['T1', 'T2', 'T3,', 'T4'] }, // uwaga: literówka celowa? zostawiam poprawną listę niżej
+      {
+        id: 'A',
+        name: 'Grupa A',
+        teamIds: [
+          'T1',
+          'T2',
+          'T3',
+          'T4',
+          'T5',
+          'T6',
+          'T7',
+          'T8',
+          'T9',
+          'T10',
+          'T11',
+          'T12',
+          'T13',
+          'T14',
+          'T15',
+          'T16',
+          'T17',
+          'T18',
+        ],
+      },
     ],
     stages: [
       { id: this.STAGE_GRP, name: 'Faza grupowa', kind: 'GROUP', order: 1 },
     ],
   };
 
-  // Poprawna lista teamIds w grupie (bez przecinka w 'T3')
   constructor() {
     this.tournament.groups = [
-      { id: 'A', name: 'Grupa A', teamIds: ['T1', 'T2', 'T3', 'T4'] },
+      {
+        id: 'A',
+        name: 'Grupa A',
+        teamIds: [
+          'T1',
+          'T2',
+          'T3',
+          'T4',
+          'T5',
+          'T6',
+          'T7',
+          'T8',
+          'T9',
+          'T10',
+          'T11',
+          'T12',
+          'T13',
+          'T14',
+          'T15',
+          'T16',
+          'T17',
+          'T18',
+        ],
+      },
     ];
   }
 
-  // MOCK: drużyny i zawodnicy
   private teams: Team[] = [
     {
       id: 'T1',
-      name: 'DP Meble',
-      logo: 'assets/logos/t1.png',
+      name: 'Lech Poznań',
+      logo: 'assets/LOGA/LECH-POZNAN-LOGO.jpg',
       playerIds: ['P1', 'P2', 'P3'],
     },
     {
       id: 'T2',
-      name: 'RKN Konin',
-      logo: 'assets/logos/t2.png',
+      name: 'Motor Lublin',
+      logo: 'assets/LOGA/MOTOR-LUBLIN-LOGO.jpg',
       playerIds: ['P4', 'P5', 'P6'],
     },
     {
       id: 'T3',
-      name: 'AKP Magros Konin',
-      logo: 'assets/logos/t3.png',
+      name: 'Arka Gdynia',
+      logo: 'assets/LOGA/ARKA-GDYNIA-LOGO.jpg',
       playerIds: ['P7', 'P8'],
     },
     {
       id: 'T4',
-      name: 'Tradycja Sarbicko',
-      logo: 'assets/logos/t4.png',
+      name: 'Widzew Łódź',
+      logo: 'assets/LOGA/WIDZEW-LODZ-LOGO.jpg',
+      playerIds: ['P9', 'P10'],
+    },
+    {
+      id: 'T5',
+      name: 'Pogoń Szczecin',
+      logo: 'assets/LOGA/POGON-SZCZECIN-LOGO.jpg',
+      playerIds: ['P9', 'P10'],
+    },
+    {
+      id: 'T6',
+      name: 'Wisła Płock',
+      logo: 'assets/LOGA/WISLA-PLOCK-LOGO.png',
+      playerIds: ['P9', 'P10'],
+    },
+    {
+      id: 'T7',
+      name: 'Jagiellonia Białystok',
+      logo: 'assets/LOGA/JAGIELONIA-BIALYSTOK-LOGO.jpg',
+      playerIds: ['P9', 'P10'],
+    },
+    {
+      id: 'T8',
+      name: 'Korona Kielce',
+      logo: 'assets/LOGA/KORONA-KIELCE-LOGO.jpg',
+      playerIds: ['P9', 'P10'],
+    },
+    {
+      id: 'T9',
+      name: 'Raków Częstochowa',
+      logo: 'assets/LOGA/RAKOW-CZESTOCHOWA-LOGO.png',
+      playerIds: ['P9', 'P10'],
+    },
+    {
+      id: 'T10',
+      name: 'Piast Gliwice',
+      logo: 'assets/LOGA/PIAST-GLIWICE-LOGO.png',
+      playerIds: ['P9', 'P10'],
+    },
+    {
+      id: 'T11',
+      name: 'Radomiak Radom',
+      logo: 'assets/LOGA/RADOMIAK-RADOM-LOGO.jpg',
+      playerIds: ['P9', 'P10'],
+    },
+    {
+      id: 'T12',
+      name: 'Cracovia',
+      logo: 'assets/LOGA/CRACOVIA-LOGO.png',
+      playerIds: ['P9', 'P10'],
+    },
+    {
+      id: 'T13',
+      name: 'GKS Katowice',
+      logo: 'assets/LOGA/GKS-KATOWICE-LOGO.jpg',
+      playerIds: ['P9', 'P10'],
+    },
+    {
+      id: 'T14',
+      name: 'KGHM Zagłębie Lubin',
+      logo: 'assets/LOGA/ZAGLEBIE-LUBIN-LOGO.jpg',
+      playerIds: ['P9', 'P10'],
+    },
+    {
+      id: 'T15',
+      name: 'Bruk-Bet Termalica Nieciecza',
+      logo: 'assets/LOGA/TERMALICA-NIECIECZA-LOGO.png',
+      playerIds: ['P9', 'P10'],
+    },
+    {
+      id: 'T16',
+      name: 'Górnik Zabrze',
+      logo: 'assets/LOGA/GORNIK-ZABRZE-LOGO.jpg',
+      playerIds: ['P9', 'P10'],
+    },
+    {
+      id: 'T17',
+      name: 'Lechia Gdańsk',
+      logo: 'assets/LOGA/LECHIA-GDANSK-LOGO.png',
+      playerIds: ['P9', 'P10'],
+    },
+    {
+      id: 'T18',
+      name: 'Legia Warszawa',
+      logo: 'assets/LOGA/LEGIA-WARSZAWA-LOGO.jpg',
       playerIds: ['P9', 'P10'],
     },
   ];
@@ -152,7 +278,6 @@ export class MockTournamentApi implements ITournamentApi {
     },
   ];
 
-  // MOCK: kilka meczów fazy grupowej (na start statycznie)
   private matches: Match[] = [
     {
       id: 'M1',
@@ -206,7 +331,6 @@ export class MockTournamentApi implements ITournamentApi {
     },
   ];
 
-  // Implementacja portu API
   getTournament() {
     return of(this.tournament);
   }
